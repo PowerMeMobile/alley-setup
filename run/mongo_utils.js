6ff1d442397bdb3f3bb70f980baf9a771d298f79
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-09-30 15:44:46 +0300

    Refactor

--- run/mongo_utils.js
+++ run/mongo_utils.js
@@ -1,59 +1,44 @@
+var mailbox_tables = [
+    "db.mb_funnel_receipts",
+    "db.mb_incoming_sms",
+    "db.mb_pending_items",
+    "db.mb_subscriptions",
+    "db.mb_k1api_input_id_to_sub_id",
+    "db.mb_k1api_receipt_subs",
+    "db.mb_k1api_receipts"
+];
+
+var config_tables = [
+    "db.customers",
+    "db.gateways",
+    "db.network_maps",
+    "db.networks",
+    "db.providers",
+    "db.mb_msisdns"
+];
+
 var mailbox_count = function() {
-    tables = [
-        "db.mb_funnel_receipts",
-        "db.mb_incoming_sms",
-        "db.mb_k1api_input_id_to_sub_id",
-        "db.mb_k1api_receipt_subs",
-        "db.mb_k1api_receipts",
-        "db.mb_pending_items",
-        "db.mb_subscriptions"
-    ];
-    tables.forEach(function(table) {
+    mailbox_tables.forEach(function(table) {
         var count = eval(table + ".count();");
         print(table + " " + count);
     });
 };
 
 var mailbox_remove = function() {
-    tables = [
-        "db.mb_funnel_receipts",
-        "db.mb_incoming_sms",
-        "db.mb_k1api_input_id_to_sub_id",
-        "db.mb_k1api_receipt_subs",
-        "db.mb_k1api_receipts",
-        "db.mb_pending_items",
-        "db.mb_subscriptions"
-    ];
-    tables.forEach(function(table) {
+    mailbox_tables.forEach(function(table) {
         eval(table + ".remove({});");
     });
 };
 
 var config_count = function() {
-    tables = [
-        "db.customers",
-        "db.gateways",
-        "db.network_maps",
-        "db.networks",
-        "db.providers",
-        "db.mb_msisdns"
-    ];
-    tables.forEach(function(table) {
+    config_tables.forEach(function(table) {
         var count = eval(table + ".count();");
         print(table + " " + count);
     });
 };
 
 var config_remove = function() {
-    tables = [
-        "db.customers",
-        "db.gateways",
-        "db.network_maps",
-        "db.networks",
-        "db.providers",
-        "db.mb_msisdns"
-    ];
-    tables.forEach(function(table) {
+    config_tables.forEach(function(table) {
         eval(table + ".remove({});");
     });
 };
